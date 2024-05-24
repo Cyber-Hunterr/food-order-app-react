374f99b566b1fe8859faf5a9d1e65ff3c36e68ed
Cyber-Hunterr <rohitmeena1786@gmail.com>
2024-05-24 12:08:13 +0530

    Added Cart Modal

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,16 @@
+import { useRef,useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+export default function Modal({children, open, className=''}) {
+    const dialogRef = useRef(); 
+    useEffect(()=>{
+        const modal = dialogRef.current;
+        if(open){
+            modal.showModal();
+        }
+        return ()=> modal.close();
+    },[open])
+    return createPortal(<dialog ref={dialogRef} className={`modal ${className}`}>
+        {children}
+    </dialog>,document.getElementById('modal'));
+}
